691acb8fe9a506c5854665cfc329d561b9dda9e9
GabrielFerrin <gabrielferrin@byui.edu>
2024-05-24 14:01:09 -0500

    update

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,9 +1,8 @@
 // evironment
-const prod = false
+const prod = true
 let url = '';
 url = prod ? 'https://funval-users.onrender.com' :
   'http://localhost:3001'
-
 window.env = window.env || {};
 window.env.API_URL = url;
 window.env.API_URL_CHECK = url + '/is-alive';
